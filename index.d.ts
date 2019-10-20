bf61cc746ca7bd80e1709d71d2e843fa683003d9
Rasmus Schultz <rasmus@mindplay.dk>
2019-10-20 12:47:37 +0200

    no type-checking for the input, but at least this works as far as knowing what comes out of the function.

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,3 @@
 declare module "json-deep-copy" {
-  export interface JsonObject {
-    [key: string]: JsonValue;
-  }
-
-  export interface JsonArray extends Array<JsonValue> {}
-
-  export type JsonValue = boolean | number | string | null | JsonArray | JsonObject;
-
-  export default function deepCopy<T extends JsonValue>(src: T): T;
+  export default function deepCopy<T>(src: T): T;
 }
